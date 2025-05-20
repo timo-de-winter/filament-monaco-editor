3f1e22a52d94ff7d53701b412aa627615a7e47ae
Timo de Winter <timo@be-interactive.nl>
2025-05-20 15:11:51 +0200

    Sets up two-way state sync

--- resources/js/components/monaco-editor.js
+++ resources/js/components/monaco-editor.js
@@ -1,45 +1,56 @@
 import * as monaco from 'monaco-editor';
 
-self.MonacoEnvironment = {
-    getWorker: function (workerId, label) {
-        const getWorkerModule = (moduleUrl, label) => {
-            return new Worker(self.MonacoEnvironment.getWorkerUrl(moduleUrl), {
-                name: label,
-                type: 'module'
-            });
-        };
+// self.MonacoEnvironment = {
+//     getWorker: function (workerId, label) {
+//         const getWorkerModule = (moduleUrl, label) => {
+//             return new Worker(self.MonacoEnvironment.getWorkerUrl(moduleUrl), {
+//                 name: label,
+//                 type: 'module'
+//             });
+//         };
+//
+//         switch (label) {
+//             case 'json':
+//                 return getWorkerModule('/monaco-editor/esm/vs/language/json/json.worker?worker', label);
+//             case 'css':
+//             case 'scss':
+//             case 'less':
+//                 return getWorkerModule('/monaco-editor/esm/vs/language/css/css.worker?worker', label);
+//             case 'html':
+//             case 'blade':
+//             case 'handlebars':
+//             case 'razor':
+//                 return getWorkerModule('/monaco-editor/esm/vs/language/html/html.worker?worker', label);
+//             case 'typescript':
+//             case 'javascript':
+//                 return getWorkerModule('/monaco-editor/esm/vs/language/typescript/ts.worker?worker', label);
+//             default:
+//                 return getWorkerModule('/monaco-editor/esm/vs/editor/editor.worker?worker', label);
+//         }
+//     }
+// };
 
-        switch (label) {
-            case 'json':
-                return getWorkerModule('/monaco-editor/esm/vs/language/json/json.worker?worker', label);
-            case 'css':
-            case 'scss':
-            case 'less':
-                return getWorkerModule('/monaco-editor/esm/vs/language/css/css.worker?worker', label);
-            case 'html':
-            case 'blade':
-            case 'handlebars':
-            case 'razor':
-                return getWorkerModule('/monaco-editor/esm/vs/language/html/html.worker?worker', label);
-            case 'typescript':
-            case 'javascript':
-                return getWorkerModule('/monaco-editor/esm/vs/language/typescript/ts.worker?worker', label);
-            default:
-                return getWorkerModule('/monaco-editor/esm/vs/editor/editor.worker?worker', label);
-        }
-    }
-};
-
-export default function testComponent({
-  state,
+export default function monacoEditor({
+    state,
+    updateUsing,
 }) {
     return {
         state,
 
         init: () => {
-            monaco.editor.create(this.$el.querySelector('#monaco-editor'), {
-                value: 'test',
-                language: 'javascript',
+            const editor = monaco.editor.create(this.$el.querySelector('#monaco-editor'), {
+                value: state.initialValue,
+                language: 'javascript', // todo: get language from editor
+            });
+
+            editor.getModel().onDidChangeContent(event => {
+                updateUsing(editor.getModel().getValue());
+            });
+
+            this.$watch('state', newState => {
+                if (newState !== editor.getModel().getValue()) {
+                    editor.getModel().setValue(newState);
+                }
             });
         },
     }
--- resources/views/filament/forms/components/monaco-editor.blade.php
+++ resources/views/filament/forms/components/monaco-editor.blade.php
@@ -2,14 +2,21 @@
     :component="$getFieldWrapperView()"
     :field="$field"
 >
-    <div
-        x-load
-        x-load-css="[@js(\Filament\Support\Facades\FilamentAsset::getStyleHref('monaco-editor-css', package: 'timo-de-winter/filament-monaco-editor'))]"
-        x-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('monaco-editor', package: 'timo-de-winter/filament-monaco-editor') }}"
-        x-data="monacoEditor({
-            state: $wire.{{ $applyStateBindingModifiers("\$entangle('{$getStatePath()}')") }},
-        })"
-    >
-        <div id="monaco-editor" style="min-height: 400px;"></div>
+    <div x-data="{ state: $wire.{{ $applyStateBindingModifiers("\$entangle('{$getStatePath()}')") }} }">
+        <div
+            x-load
+            x-load-css="[@js(\Filament\Support\Facades\FilamentAsset::getStyleHref('monaco-editor-css', package: 'timo-de-winter/filament-monaco-editor'))]"
+            x-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('monaco-editor', package: 'timo-de-winter/filament-monaco-editor') }}"
+            x-data="monacoEditor({
+                state: $wire.{{ $applyStateBindingModifiers("\$entangle('{$getStatePath()}')") }},
+                updateUsing: (newState) => {
+                    state = newState;
+                }
+            })"
+            wire:ignore
+        >
+            <div id="monaco-editor" style="min-height: 400px;"></div>
+        </div>
     </div>
+
 </x-dynamic-component>
