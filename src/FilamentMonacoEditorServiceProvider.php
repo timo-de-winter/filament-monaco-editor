a6bc5404c3fdeb864a5a5531b88e32711ebca846
Timo de Winter <timo@be-interactive.nl>
2025-05-20 11:12:48 +0200

    Some minor changes

--- src/FilamentMonacoEditorServiceProvider.php
+++ src/FilamentMonacoEditorServiceProvider.php
@@ -9,15 +9,10 @@ class FilamentMonacoEditorServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('filament-monaco-editor')
             ->hasConfigFile()
             ->hasViews()
-            ->hasMigration('create_filament_monaco_editor_table');
+            ->hasMigration('create_filament_monaco_editor_tables');
     }
 }
