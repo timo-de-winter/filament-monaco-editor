42f80266ffb0f6a569599fc4995f8f378329e711
Timo de Winter <timo@be-interactive.nl>
2025-05-20 11:25:03 +0200

    Adds custom field

---
+++ resources/views/filament/forms/components/monaco-editor.blade.php
@@ -0,0 +1,8 @@
+<x-dynamic-component
+    :component="$getFieldWrapperView()"
+    :field="$field"
+>
+    <div x-data="{ state: $wire.{{ $applyStateBindingModifiers("\$entangle('{$getStatePath()}')") }} }">
+        <input x-model="state" />
+    </div>
+</x-dynamic-component>
---
+++ src/Filament/Forms/Components/MonacoEditor.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace TimoDeWinter\FilamentMonacoEditor\Filament\Forms\Components;
+
+use Filament\Forms\Components\Field;
+
+class MonacoEditor extends Field
+{
+    protected string $view = 'filament-monaco-editor::filament.forms.components.monaco-editor';
+}
