ef373d2f2568c7c8a40a5a821cfb99ca6401a81d
Timo de Winter <timo@be-interactive.nl>
2025-05-20 15:15:45 +0200

    Ignore view-string

--- src/Filament/Forms/Components/MonacoEditor.php
+++ src/Filament/Forms/Components/MonacoEditor.php
@@ -6,5 +6,6 @@ use Filament\Forms\Components\Field;
 
 class MonacoEditor extends Field
 {
+    // @phpstan-ignore-next-line
     protected string $view = 'filament-monaco-editor::filament.forms.components.monaco-editor';
 }
