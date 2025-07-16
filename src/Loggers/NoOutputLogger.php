fb4c8e4f3ac48a9640bea15a6de607fc07db4f99
Timo de Winter <info@timodw.nl>
2025-07-16 13:59:18 +0200

    Fixes log output

---
+++ src/Loggers/NoOutputLogger.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace TimoDeWinter\FilamentMonacoEditor\Loggers;
+
+use ScssPhp\ScssPhp\Deprecation;
+use ScssPhp\ScssPhp\Logger\LoggerInterface;
+use ScssPhp\ScssPhp\StackTrace\Trace;
+use SourceSpan\FileSpan;
+use SourceSpan\SourceSpan;
+
+class NoOutputLogger implements LoggerInterface
+{
+    public function warn(string $message, ?Deprecation $deprecation = null, ?FileSpan $span = null, ?Trace $trace = null): void
+    {
+        return;
+    }
+
+    /**
+     * Emits a debugging message associated with the given span.
+     */
+    public function debug(string $message, SourceSpan $span): void
+    {
+        return;
+    }
+}
